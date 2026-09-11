54e99d632d5fe442c0c5b385c9778cbbe90b21fd
Zaid Bin Ather <zaid.athar.2009@gmail.com>
2026-09-11 15:01:03 +0500

    Fix req.path for route matching

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ app.use("/uploads", express.static(path.join(__dirname, "../server/uploads")));
 app.use((req, res, next) => {
     if (req.path.startsWith("/api/")) {
         req.url = req.path.replace("/api", "");
+        req.path = req.url;
         next();
     } else {
         res.sendFile(path.join(__dirname, "../client/dist/index.html"));
